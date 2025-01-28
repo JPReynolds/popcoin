79f736d692c360cb9d480e2ce0863cfee0100cb2
JPReynolds <58976714+JPReynolds@users.noreply.github.com>
2025-01-28 17:27:32 +0000

    redirect from auth pages

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,22 @@
-export { auth as middleware } from "@/auth"
+import { auth } from "@/auth";
+import { Session } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+interface NextAuthRequest extends NextRequest {
+  auth: Session | null;
+}
+
+export default auth(async (request: NextAuthRequest) => {
+  const session = request.auth;
+  const pathname = request.nextUrl.pathname;
+
+  if (session && ["/sign-up", "sign-in"].includes(pathname)) {
+    return Response.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/sign-up", "/sign-in"],
+};
