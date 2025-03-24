c89447a36703a0e377fa1bb1864edeeecd70e76d
JPReynolds <58976714+JPReynolds@users.noreply.github.com>
2025-03-24 20:21:11 +0000

    await params

--- app/(media)/series/popular/page.tsx
+++ app/(media)/series/popular/page.tsx
@@ -2,12 +2,11 @@ import { Suspense } from "react";
 import { MediaListSkeleton } from "@/components/media-list-skeleton";
 import { PopularSeries } from "@/components/popular-series";
 
-export default function Popular({
-  searchParams,
-}: {
-  searchParams: { page?: string };
+export default async function Popular(props: {
+  searchParams: Promise<{ page?: string }>;
 }) {
-  const page = searchParams.page ? parseInt(searchParams.page) : 1;
+  const searchParams = await props.searchParams;
+  const page = searchParams?.page ? parseInt(searchParams.page) : 1;
 
   return (
     <div className="flex flex-col justify-center">
